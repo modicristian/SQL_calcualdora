ef63fcb8de53927ce8ff84a2dc5113317c4882ab
modicristian <cristianmodi@gmail.com>
2018-08-28 20:24:05 -0300

    modificaciones

--- app/src/main/java/com/example/itmaster/sql_calculadora/interfaz/ListActivity.java
+++ app/src/main/java/com/example/itmaster/sql_calculadora/interfaz/ListActivity.java
@@ -2,14 +2,30 @@ package com.example.itmaster.sql_calculadora.interfaz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.itmaster.sql_calculadora.DAO.SqliteCalculadora;
 import com.example.itmaster.sql_calculadora.R;
+import com.example.itmaster.sql_calculadora.adapter.AdapterListOperaciones;
+import com.example.itmaster.sql_calculadora.models.Operacion;
 
-public class ListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity
+{
+
+    private SqliteCalculadora sqliteCalculadora;
+    private AdapterListOperaciones listOperaciones;
+    private ListView listView;
+    private Operacion Op;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        
+
     }
 }
--- app/src/main/java/com/example/itmaster/sql_calculadora/listeners/ListenerHistorial.java
+++ app/src/main/java/com/example/itmaster/sql_calculadora/listeners/ListenerHistorial.java
@@ -1,19 +1,24 @@
 package com.example.itmaster.sql_calculadora.listeners;
 
+import android.content.Intent;
 import android.view.View;
 
+import com.example.itmaster.sql_calculadora.interfaz.ListActivity;
 import com.example.itmaster.sql_calculadora.interfaz.MainActivity;
 
 public class ListenerHistorial implements View.OnClickListener
 {
     private MainActivity contextHistorial;
 
-    public ListenerHistorial(MainActivity contextHistorial) {
+    public ListenerHistorial(MainActivity contextHistorial)
+    {
         this.contextHistorial = contextHistorial;
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
+        Intent historial = new Intent(contextHistorial, ListActivity.class);
 
     }
 }
